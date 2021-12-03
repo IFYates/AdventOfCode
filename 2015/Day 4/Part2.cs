bedd51da1f2f700e173a2d532e0927cd863d7349
IFYates <ian@iyates.co.uk>
2021-12-03 19:15:16 +0000

    Day 4 (bruteforce)

---
+++ 2015/Day 4/Part2.cs
@@ -0,0 +1,15 @@
+var prefix = Console.In.ReadToEnd();
+
+var algo = System.Security.Cryptography.MD5.Create();
+var i = 0;
+while (++i <= int.MaxValue)
+{
+    if (i % 10000 == 0) { Console.WriteLine($"{i}"); }
+    var bytes = Encoding.Default.GetBytes($"{prefix}{i}");
+    var hash = algo.ComputeHash(bytes);
+    if (hash[0] == 0 & hash[1] == 0 && hash[2] == 0)
+    {
+        Console.WriteLine($"> {prefix} {i}");
+        break;
+    }
+}
