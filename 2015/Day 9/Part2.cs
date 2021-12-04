d0771a8e81bf5b5c5c7f4676535299e5d11fc2cd
IFYates <ian@iyates.co.uk>
2021-12-04 16:21:08 +0000

    2015 Day 9

---
+++ 2015/Day 9/Part2.cs
@@ -0,0 +1,80 @@
+static Dictionary<string, Node> nodes = new();
+static Node getNode(string name)
+{
+    if (!nodes.TryGetValue(name, out var node))
+    {
+        node = new Node(name);
+        nodes[name] = node;
+    }
+    return node;   
+}
+
+record Node(string Name)
+{
+    public Dictionary<string, int> Distances { get; } = new();
+}
+class Route
+{
+    public Node Node { get; init; }
+    public int Steps { get; init; }
+    public int Distance { get; init; }
+    public Route Previous { get; init; }
+    public string[] Path { get; init; }
+
+    public Route Map()
+    {
+        Route longest = null;
+        foreach (var next in nodes.Keys.Where(k => !Path.Contains(k)))
+        {
+            var newPath = new List<string>(Path);
+            newPath.Add(next);
+
+            var r = new Route
+            {
+                Node = getNode(next),
+                Steps = Steps + 1,
+                Distance = Distance + Node.Distances[next],
+                Previous = this,
+                Path = newPath.ToArray()
+            };
+            var s = r.Map();
+            if (longest == null || s.Distance > longest.Distance)
+            {
+                longest = s;
+            }
+        }
+        return longest ?? this;
+    }
+}
+
+var ln = Console.In.ReadLine();
+while (ln != null)
+{
+    var m = System.Text.RegularExpressions.Regex.Match(ln, @"^(\w+) to (\w+) = (\d+)$");
+    if (!m.Success)
+    {
+        Console.Error.WriteLine("Bad line: " + ln);
+        break;
+    }
+
+    var lNode = getNode(m.Groups[1].Value);
+    var rNode = getNode(m.Groups[2].Value);
+    lNode.Distances[rNode.Name] = int.Parse(m.Groups[3].Value);
+    rNode.Distances[lNode.Name] = int.Parse(m.Groups[3].Value);
+
+    ln = Console.In.ReadLine();
+}
+
+Route longest = null;
+foreach (var node in nodes.Values)
+{
+    var r = new Route { Node = node, Path = new[] { node.Name } };
+    var s = r.Map();
+    if (longest == null || s.Distance > longest.Distance)
+    {
+        longest = s;
+    }
+}
+
+//Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(nodes));
+Console.WriteLine("> " + longest?.Distance);
