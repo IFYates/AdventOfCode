a4ee1b9e153f4500d3c23d6e909e6dbaa51f2905
IFYates <ian@iyates.co.uk>
2021-12-05 00:51:10 +0000

    2015 Day 10

---
+++ 2015/Day 10/Part2.cs
@@ -0,0 +1,22 @@
+static string lookSay(string str)
+{
+    var result = "";
+    for (var i = 0; i < str.Length; ++i)
+    {
+        var c = str[i];
+        var j = i + 1;
+        for (; j < str.Length && str[j] == c; ++j);
+        result += $"{j - i}{c}";
+        i = j - 1;
+    }
+    return result;
+}
+
+var ln = Console.In.ReadToEnd();
+for (var i = 0; i < 50; ++i)
+{
+    Console.WriteLine($"> {i} ({ln.Length})");
+    ln = lookSay(ln);
+}
+
+Console.WriteLine($"> {ln.Length}");
