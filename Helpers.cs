2719c3c6fb4ed0a1bee963339c224a2442daaee6
IFYates <ian@iyates.co.uk>
2021-12-26 10:44:19 +0000

    2021 Day 15 Part 2

---
+++ Helpers.cs
@@ -0,0 +1,19 @@
+﻿namespace AOC;
+public static class Helpers
+{
+    public static T[] ArrayOf<T>(int length, Func<T> fact)
+    {
+        return Enumerable.Range(1, length).Select(i => fact()).ToArray();
+    }
+
+    private static readonly Dictionary<string, int> _printTimes = new();
+    public static void PrintPerSecond(string template, params object[] args)
+    {
+        var secs = (int)DateTime.UtcNow.TimeOfDay.TotalSeconds;
+        if (!_printTimes.TryGetValue(template, out var last) || last < secs)
+        {
+            _printTimes[template] = secs;
+            Console.WriteLine(string.Format(template, args));
+        }
+    }
+}
